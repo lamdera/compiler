56e2b6e371225d8aed3df66a25d990cc8d62a049
Mario Rogic <hello@mario.net.au>
2018-07-20 13:05:24 +0100

    Minimise what we need to generate

--- extra/src/AllTypes_Gen.elm
+++ extra/src/AllTypes_Gen.elm
@@ -1,6 +1,7 @@
 module AllTypes_Gen exposing (..)
 
 import AllTypes exposing (..)
+import Evergreen as EG
 import Json.Decode as D
 import Json.Encode as E
 import Set
@@ -28,20 +29,15 @@ import Set
 evg_e_AllTypes : AllTypes -> E.Value
 evg_e_AllTypes evg_p0 =
     E.list identity
-        [ E.string ""
-        , E.int evg_p0.int
+        [ E.int evg_p0.int
         , E.float evg_p0.float
         , E.bool evg_p0.bool
-        , evg_e_Char evg_p0.char
+        , EG.e_Char evg_p0.char
         , E.string evg_p0.string
         , E.list E.int evg_p0.listInt
         , E.set E.float evg_p0.setFloat
         , E.array E.string evg_p0.arrayString
-
-        -- , E.dict String.fromInt E.string evg_p0.dictIntString
-        , evg_e_Order evg_p0.order
-
-        -- , evg_e_RecursiveUnion evg_p0.union
+        , EG.e_Order evg_p0.order
         ]
 
 
@@ -51,99 +47,13 @@ evg_d_AllTypes =
         |> D.andThen
             (\v ->
                 D.succeed AllTypes
-                    |> evg_atIndex 1 D.int
-                    |> evg_atIndex 2 D.float
-                    |> evg_atIndex 3 D.bool
-                    |> evg_atIndex 4 evg_d_Char
-                    |> evg_atIndex 5 D.string
-                    |> evg_atIndex 6 (D.list D.int)
-                    |> evg_atIndex 7 (D.list D.float |> D.andThen (\evg_v0 -> D.succeed <| Set.fromList evg_v0))
-                    |> evg_atIndex 8 (D.array D.string)
-                    -- Dicts are kinda tricky... how do we rep non-string keys? In 0.19 any type will be Dict Keyable...
-                    -- |> evg_atIndex 9 (D.dict D.string |> D.andThen (\evg_v0 ->  ))
-                    |> evg_atIndex 10 evg_d_Order
+                    |> EG.atIndex 0 D.int
+                    |> EG.atIndex 1 D.float
+                    |> EG.atIndex 2 D.bool
+                    |> EG.atIndex 3 EG.d_Char
+                    |> EG.atIndex 4 D.string
+                    |> EG.atIndex 5 (D.list D.int)
+                    |> EG.atIndex 6 (EG.d_set D.float)
+                    |> EG.atIndex 7 (D.array D.string)
+                    |> EG.atIndex 8 EG.d_Order
             )
-
-
-
--- Move elsewhere later
-
-
-evg_e_Char : Char -> E.Value
-evg_e_Char evg_p0 =
-    E.int (Char.toCode evg_p0)
-
-
-evg_d_Char : D.Decoder Char
-evg_d_Char =
-    D.int |> D.map (\evg_v0 -> Char.fromCode evg_v0)
-
-
-evg_e_Order : Order -> E.Value
-evg_e_Order evg_p0 =
-    case evg_p0 of
-        LT ->
-            E.string "LT"
-
-        EQ ->
-            E.string "EQ"
-
-        GT ->
-            E.string "GT"
-
-
-evg_d_Order : D.Decoder Order
-evg_d_Order =
-    D.string
-        |> D.andThen
-            (\s ->
-                case s of
-                    "LT" ->
-                        D.succeed LT
-
-                    "EQ" ->
-                        D.succeed EQ
-
-                    "GT" ->
-                        D.succeed GT
-
-                    _ ->
-                        D.fail <| "unexpected Order value: " ++ s
-            )
-
-
-eqPosStr : Int -> String -> a -> D.Decoder a
-eqPosStr idx str final =
-    D.index idx D.string
-        |> D.andThen
-            (\s ->
-                if s == str then
-                    D.succeed final
-                else
-                    D.fail "evergreen wire decode failed for union type"
-            )
-
-
-eqPosStr1 : Int -> String -> D.Decoder a -> (a -> b) -> D.Decoder b
-eqPosStr1 idx str subDecoder constructor =
-    D.index idx D.string
-        |> D.andThen
-            (\s ->
-                if s == str then
-                    subDecoder |> D.andThen (\v1 -> D.succeed <| constructor v1)
-                else
-                    D.fail "evergreen wire decode failed for union type"
-            )
-
-
-evg_atIndex idx decoder =
-    evg_custom (D.index idx decoder)
-
-
-
--- Trick from json-decode-pipeline
-
-
-evg_custom : D.Decoder a -> D.Decoder (a -> b) -> D.Decoder b
-evg_custom =
-    D.map2 (|>)
--- extra/src/Evergreen.elm
+++ extra/src/Evergreen.elm
@@ -1,6 +1,119 @@
 module Evergreen exposing (..)
 
 import Json.Decode as D
+import Json.Encode as E
+import Set exposing (Set)
+
+
+-- eqPosStr : Int -> String -> a -> D.Decoder a
+-- eqPosStr idx str final =
+--     D.index idx D.string
+--         |> D.andThen
+--             (\s ->
+--                 if s == str then
+--                     D.succeed final
+--                 else
+--                     D.fail "wire decode failed for Msg"
+--             )
+--
+--
+-- eqPosStr1 : Int -> String -> D.Decoder a -> (a -> b) -> D.Decoder b
+-- eqPosStr1 idx str subDecoder constructor =
+--     D.index idx D.string
+--         |> D.andThen
+--             (\s ->
+--                 if s == str then
+--                     subDecoder |> D.andThen (\v1 -> D.succeed <| constructor v1)
+--                 else
+--                     D.fail "wire decode failed for Msg"
+--             )
+--
+--
+-- objStringAt : List String -> D.Decoder a -> D.Decoder a
+-- objStringAt fields decoder =
+--     D.at fields D.string
+--         |> D.map (D.decodeString decoder)
+--         |> D.andThen
+--             (\r ->
+--                 case r of
+--                     Err x ->
+--                         D.fail x
+--
+--                     Ok x ->
+--                         D.succeed x
+--             )
+--
+--
+-- decodeAny : String -> (String -> D.Decoder a) -> a -> a
+-- decodeAny string chainDecoder default =
+--     D.decodeString
+--         (D.field "version" D.string
+--             |> D.andThen chainDecoder
+--         )
+--         string
+--         -- |> Debug.log "decodeAny"
+--         |> Result.withDefault default
+--
+--
+-- decodeAnyM : String -> (String -> D.Decoder a) -> a -> a
+-- decodeAnyM string chainDecoderM default =
+--     D.decodeString
+--         (D.index 0 D.string
+--             |> D.andThen chainDecoderM
+--         )
+--         string
+--         |> Result.withDefault default
+--
+--
+-- Move elsewhere later
+
+
+e_Char : Char -> E.Value
+e_Char evg_p0 =
+    E.int (Char.toCode evg_p0)
+
+
+d_Char : D.Decoder Char
+d_Char =
+    D.int |> D.map (\evg_v0 -> Char.fromCode evg_v0)
+
+
+e_Order : Order -> E.Value
+e_Order evg_p0 =
+    case evg_p0 of
+        LT ->
+            E.string "LT"
+
+        EQ ->
+            E.string "EQ"
+
+        GT ->
+            E.string "GT"
+
+
+d_Order : D.Decoder Order
+d_Order =
+    D.string
+        |> D.andThen
+            (\s ->
+                case s of
+                    "LT" ->
+                        D.succeed LT
+
+                    "EQ" ->
+                        D.succeed EQ
+
+                    "GT" ->
+                        D.succeed GT
+
+                    _ ->
+                        D.fail <| "unexpected Order value: " ++ s
+            )
+
+
+d_set : D.Decoder comparable -> D.Decoder (Set comparable)
+d_set decoder =
+    D.list decoder |> D.map Set.fromList
 
 
 eqPosStr : Int -> String -> a -> D.Decoder a
@@ -11,7 +124,7 @@ eqPosStr idx str final =
                 if s == str then
                     D.succeed final
                 else
-                    D.fail "wire decode failed for Msg"
+                    D.fail "evergreen wire decode failed for union type"
             )
 
 
@@ -23,41 +136,18 @@ eqPosStr1 idx str subDecoder constructor =
                 if s == str then
                     subDecoder |> D.andThen (\v1 -> D.succeed <| constructor v1)
                 else
-                    D.fail "wire decode failed for Msg"
+                    D.fail "evergreen wire decode failed for union type"
             )
 
 
-objStringAt : List String -> D.Decoder a -> D.Decoder a
-objStringAt fields decoder =
-    D.at fields D.string
-        |> D.map (D.decodeString decoder)
-        |> D.andThen
-            (\r ->
-                case r of
-                    Err x ->
-                        D.fail x
-
-                    Ok x ->
-                        D.succeed x
-            )
+atIndex idx decoder =
+    custom (D.index idx decoder)
 
 
-decodeAny : String -> (String -> D.Decoder a) -> a -> a
-decodeAny string chainDecoder default =
-    D.decodeString
-        (D.field "version" D.string
-            |> D.andThen chainDecoder
-        )
-        string
-        -- |> Debug.log "decodeAny"
-        |> Result.withDefault default
+
+-- Trick from json-decode-pipeline
 
 
-decodeAnyM : String -> (String -> D.Decoder a) -> a -> a
-decodeAnyM string chainDecoderM default =
-    D.decodeString
-        (D.index 0 D.string
-            |> D.andThen chainDecoderM
-        )
-        string
-        |> Result.withDefault default
+custom : D.Decoder a -> D.Decoder (a -> b) -> D.Decoder b
+custom =
+    D.map2 (|>)
