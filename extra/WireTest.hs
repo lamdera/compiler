737f201e2461107fc6fc146609c1191e1a41bd3c
Mario Rogic <hello@mario.net.au>
2018-08-02 16:45:56 +0100

    Cycle instructions

--- extra/WireTest.hs
+++ extra/WireTest.hs
@@ -12,6 +12,40 @@ import qualified Reporting.Task as Task
 import qualified Reporting.Progress.Terminal as Terminal
 
 
+{-
+
+This is a modified clone of ui/terminal/src/Develop/StaticFiles/Build.hs
+specifically to help with the development cycle for Evergreen.
+
+Changes are:
+- Generates `Output.Html` instead of `Output.Javascript` (so we can just refresh browser after run)
+- Uses `Output.Dev` instead of `Output.Prod` to avoid errors associated with Debug usage in AllTypes_Check
+
+
+Here's how to use it:
+
+1. Put the following into ~/.ghci
+
+:set -fbyte-code
+:set -fobject-code
+:set prompt "\ESC[34mλ: \ESC[m"
+
+Last line is optional, but it's cool! Lambda prompt!
+
+2. Run `stack ghci`
+
+3. λ: WireTest.compile
+   Success!
+
+4. If you've made changes, use `:r` to reload:
+
+   λ: :r
+   [169 of 169] Compiling WireTest         ( /Users/mario/dev/projects/elm-compiler/extra/WireTest.hs, /Users/mario/dev/projects/elm-compiler/.stack-work/odir/WireTest.o )
+   Ok, 169 modules loaded.
+
+-}
+
+
 -- COMPILE
 
 
