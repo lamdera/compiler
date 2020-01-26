6a34c373f7f4d10b9743bebc8b0d273577e8b195
Mario Rogic <hello@mario.net.au>
2020-01-26 12:51:46 +0000

    Missing file

---
+++ extra/LamderaHints.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module LamderaHints where
+
+import qualified Reporting.Doc as D
+import qualified Elm.Package as Pkg
+import AST.Module.Name (Canonical(..))
+import qualified Type.Error as T
+
+
+import Data.Monoid ((<>))
+import qualified Debug.Trace as DT
+import Lamdera
+
+
+
+contextHintsWhenTypeMismatch tipe =
+  case tipe of
+    -- @TODO fix when we move this to core
+    (T.Type (Canonical (Pkg.Name "author" "project") "Evergreen.Migrate") "UnimplementedMigration" []) ->
+      -- debugTrace ("contextHintsWhenTypeMismatch: " ++ show tipe )
+        [ D.toSimpleHint $
+           "I need you to implement migrations for changed types\
+            \ as described in <https://dashboard.lamdera.app/docs/evergreen>"
+        ]
+    _ ->
+      []
