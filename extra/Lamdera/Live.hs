17e51224ddf70dfb7fa52deecc448a6dd5eed54e
Mario Rogic <hello@mario.net.au>
2020-08-07 19:54:30 +0100

    WIP network info

--- extra/Lamdera/Live.hs
+++ extra/Lamdera/Live.hs
@@ -37,6 +37,7 @@ import qualified Network.WebSockets.Snap as WS
 import SocketServer
 import Data.Word (Word8)
 import System.Process
+import Network.Info
 
 -- Bytes
 import System.Entropy
@@ -452,3 +453,18 @@ error400 s =
   do  modifyResponse $ setResponseStatus 400 "Bad Request"
       modifyResponse $ setContentType "text/html; charset=utf-8"
       writeBuilder $ "error: " <> s
+
+
+network = do
+  ns <- getNetworkInterfaces
+
+  ns
+    & fmap ipv4
+    & filter (\v -> show v /= "0.0.0.0")
+    & filter (\v -> show v /= "127.0.0.1")
+    & mapM_ (putStr . show)
+
+showInterface n = name n ++ "\n"
+               ++ "  IPv4: " ++ show (ipv4 n) ++ "\n"
+               ++ "  IPv6: " ++ show (ipv6 n) ++ "\n"
+               ++ "  MAC:  " ++ show (mac n) ++ "\n"
