587813bc38ffe1870259f45d8773deb8857f7641
Mario Rogic <hello@mario.net.au>
2023-04-29 11:04:12 +0100

    First pass of npm package setup based on @lydell/elm

---
+++ extra/npm/binary.js
@@ -0,0 +1,124 @@
+var fs = require('fs');
+var package = require('../../installers/npm/package.json');
+var path = require('path');
+
+
+
+// MAIN
+//
+// This function is used by install.js and by the bin/lamdera backup that gets
+// called when --ignore-scripts is enabled.
+
+
+module.exports = function()
+{
+	// figure out package of binary
+	var version = package.version.replace(/^(\d+\.\d+\.\d+).*$/, '$1'); // turn '1.2.3-alpha' into '1.2.3'
+	var subPackageName = '@lamdera/compiler_' + process.platform + '_' + process.arch;
+
+	verifyPlatform(version, subPackageName);
+
+	var fileName = process.platform === 'win32' ? 'lamdera.exe' : 'lamdera';
+
+	try
+	{
+		var subBinaryPath = require.resolve(subPackageName + '/' + fileName);
+	}
+	catch (error)
+	{
+		if (error && error.code === 'MODULE_NOT_FOUND')
+		{
+			exitFailure(version, missingSubPackageHelp(subPackageName));
+		}
+		else
+		{
+			exitFailure(version, 'I had trouble requiring the binary package for your platform (' + subPackageName + '):\n\n' + error);
+		}
+	}
+
+	// Yarn 2 and later ("Berry") always invokes `node` (regardless of configuration)
+	// so we cannot do any optimizations there
+	var isYarnBerry = /\byarn\/(?!1\.)/.test(process.env.npm_config_user_agent || "");
+
+	// as mentioned in bin/lamdera we cannot do any optimizations on Windows
+	if (process.platform === 'win32' || isYarnBerry)
+	{
+		return subBinaryPath;
+	}
+
+	// figure out where to put the binary
+	var binaryPath = path.resolve(__dirname, package.bin.lamdera);
+	var tmpPath = binaryPath + '.tmp';
+
+	// optimize by replacing the JS bin/lamdera with the native binary directly
+	try
+	{
+		// atomically replace the file with a hard link to the binary
+		fs.linkSync(subBinaryPath, tmpPath);
+		fs.renameSync(tmpPath, binaryPath);
+	}
+	catch (error)
+	{
+		exitFailure(version, 'I had some trouble writing file to disk. It is saying:\n\n' + error);
+	}
+
+	return binaryPath;
+}
+
+
+
+// VERIFY PLATFORM
+
+
+function verifyPlatform(version, subPackageName)
+{
+	if (subPackageName in package.optionalDependencies) return;
+
+	var situation = process.platform + '_' + process.arch;
+	console.error(
+		'-- ERROR -----------------------------------------------------------------------\n\n'
+		+ 'The @lamdera/compiler npm package does not support your platform (' + situation + ').\n\n'
+		+ 'You can try to manually download an appropriate binary (if there is one) from:\n'
+		+ 'https://dashboard.lamdera.app/docs/download\n\n'
+		+ 'Or otherwise asking for help on the Lamdera Discord:\n'
+		+ 'https://dashboard.lamdera.app/docs/discuss\n\n'
+		+ '--------------------------------------------------------------------------------\n'
+	);
+	process.exit(1);
+}
+
+
+
+// EXIT FAILURE
+
+
+function exitFailure(version, message)
+{
+	console.error(
+		'-- ERROR -----------------------------------------------------------------------\n\n'
+		+ message
+		+ '\n\nNOTE: You can avoid npm entirely by downloading directly from:\n'
+		+ 'https://dashboard.lamdera.app/docs/download\n'
+		+ 'All this package does is distribute a file from there.\n\n'
+		+ '--------------------------------------------------------------------------------\n'
+	);
+	process.exit(1);
+}
+
+
+
+// MISSING SUB PACKAGE HELP
+
+
+function missingSubPackageHelp(subPackageName)
+{
+	return (
+		'I support your platform, but I could not find the binary package (' + subPackageName + ') for it!\n\n'
+		+ 'This can happen if you use the "--omit=optional" (or "--no-optional") npm flag.\n'
+		+ 'The "optionalDependencies" package.json feature is used by Lamdera to install the correct\n'
+		+ 'binary executable for your current platform. Remove that flag to use Lamdera.\n\n'
+		+ 'This can also happen if the "node_modules" folder was copied between two operating systems\n'
+		+ 'that need different binaries - including "virtual" operating systems like Docker and WSL.\n'
+		+ 'If so, try installing with npm rather than copying "node_modules".'
+	);
+}
---
+++ extra/npm/readme.md
@@ -0,0 +1,55 @@
+# Lamdera NPM Installer
+
+[Lamdera](https://lamdera.com) is a delightful platform for full-stack web apps built with [Elm](https://elm-lang.org/).
+
+The Lamdera compiler is a super-set of the Elm compiler and a drop-in replacement: you can use `lamdera` on your existing non-Lamdera Elm projects in the same way you use `elm`.
+
+Beware of [Elm IDE gotchas](https://dashboard.lamdera.app/docs/ides-and-tooling), you'll need to set Lamdera as your Elm compiler.
+
+
+<br/>
+
+## What is this package for?
+
+**Normal installs** ❌
+
+Use the instructions [here](https://dashboard.lamdera.app/docs/download) instead.
+
+Lamdera has no dependency on `npm` to develop locally, so there's no reason to introduce it unless you specifically want to use `npm` for other reasons.
+
+**Tooling author** ✅
+
+If you're a tooling author, use Lamdera as a dependency and npm as a distribution method, then you can use this package to manage the dependency and installation.
+
+```
+npm install --save-dev @lamdera/latest
+```
+
+**Multiple versions** ⚠️
+
+If you're a tooling author, use Lamdera as a dependency and npm as a distribution method, then you can
+
+You could also use this package to use different versions of Lamdera in different projects. I.e. `npm install @lamdera/latest` in each project and use the binary at `./node_modules/.bin/lamdera` for compilation. However this is generally not recommended. Lamdera users should always use the latest release, as that's what is used when you do a `lamdera deploy`.
+
+
+**Continuous integration** ⚠️
+
+This works, but there are usually faster and more reliable options:
+
+1. You can download `lamdera` directly from [the downloads page](https://dashboard.lamdera.app/docs/download). This is all the `npm` installer does, but with extra HTTP requests to `npmjs.com` servers, making it slower and adding more failure points.
+2. Many continuous integration pipelines ways to cache files ([example](https://docs.travis-ci.com/user/caching/)) to make builds faster and more reliable.
+
+That said, it will definitely work to use the `npm` installer on CI if you prefer that option.
+
+
+<br/>
+
+## Install Locally
+
+The following command should download the latest Lamdera binary:
+
+```
+npm install @lamdera/latest
+```
+
+You should be able to run `npx lamdera --version` within your project and see `0.19.1` (the base Elm version), or `--version-full` to see the full Elm + Lamdera release version. Now you can develop with `npx lamdera live`.
