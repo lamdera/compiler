9612b171f2a38a391de5d97f3aaac6626c3e6ed6
Mario Rogic <hello@mario.net.au>
2023-09-05 08:46:31 +0100

    npm publish script helper

---
+++ extra/npm/publish-all.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -ex
+
+# The publish will fail if the package name and version combination already exists in the specified registry.
+
+version="1.2.0"
+versionFull="0.19.1-$version"
+return=$(pwd)
+
+cd packages/lamdera-darwin-arm64
+curl https://static.lamdera.com/bin/lamdera-$version-macos-arm64 -o lamdera
+chmod a+x lamdera
+npm publish --access public
+cd "$return"
+
+cd packages/lamdera-darwin-x64
+curl https://static.lamdera.com/bin/lamdera-$version-macos-x86_64 -o lamdera
+chmod a+x lamdera
+npm publish --access public
+cd "$return"
+
+cd packages/lamdera-linux-arm64
+curl https://static.lamdera.com/bin/lamdera-$version-linux-arm64 -o lamdera
+chmod a+x lamdera
+npm publish --access public
+cd "$return"
+
+cd packages/lamdera-linux-x64
+curl https://static.lamdera.com/bin/lamdera-$version-linux-x86_64 -o lamdera
+chmod a+x lamdera
+npm publish --access public
+cd "$return"
+
+cd packages/lamdera-win32-x64
+curl https://static.lamdera.com/bin/lamdera-$version-win-x86_64.zip -o lamdera.zip
+unzip -o lamdera.zip
+mv lamdera/* ./
+chmod a+x lamdera.exe
+rm -rf lamdera.zip lamdera
+npm publish --access public
+cd "$return"
+
+
+exit
+
+# When we have an arm32 bit build working we can publish this
+# cd packages/lamdera-linux-arm
+# curl https://static.lamdera.com/bin/lamdera-$version-linux-arm -o lamdera
+# chmod a+x lamdera
+# npm publish --access public
+# cd "$return" || exit
