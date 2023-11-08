f50ba1d4c30f58be33dc8ea3a43becc7d099a7bf
Mario Rogic <hello@mario.net.au>
2023-11-08 16:20:49 +0000

    MacOS thinks overlapping, Windows thinks non-exhaustive...

--- extra/Lamdera/Evaluate/Optimized.hs
+++ extra/Lamdera/Evaluate/Optimized.hs
@@ -139,12 +139,10 @@ evalExpr expr locals globals =
 
 
     (Call (VarGlobal (Global (Module.Canonical (Name "elm" "core") "Basics") "eq")) [arg1, arg2]) ->
-      case [evalExpr arg1 locals globals, evalExpr arg2 locals globals] of
-        [a_, b] -> Bool $ a_ == b
+      Bool $ evalExpr arg1 locals globals == evalExpr arg2 locals globals
 
     (Call (VarGlobal (Global (Module.Canonical (Name "elm" "core") "Basics") "neq")) [arg1, arg2]) ->
-      case [evalExpr arg1 locals globals, evalExpr arg2 locals globals] of
-        [a_, b] -> Bool $ a_ /= b
+      Bool $ evalExpr arg1 locals globals /= evalExpr arg2 locals globals
 
     (Call (VarGlobal (Global (Module.Canonical (Name "elm" "core") "Basics") "append")) [arg1, arg2]) ->
       case [evalExpr arg1 locals globals, evalExpr arg2 locals globals] of
