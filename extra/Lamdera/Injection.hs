e71514965785ed9f1e8ef70e7ffea139bda499fc
Simon Lydell <simon.lydell@gmail.com>
2024-07-07 18:11:57 +0200

    More elaborate injection for frontend upgrades

--- extra/Lamdera/Injection.hs
+++ extra/Lamdera/Injection.hs
@@ -166,11 +166,6 @@ injections isBackend isLocalDev =
 
     previousVersion = show_ previousVersionInt
 
-    isBackend_ =
-      if isBackend
-        then "true"
-        else "false"
-
     runUpdate =
       if isLocalDev
         then
@@ -178,29 +173,24 @@ injections isBackend isLocalDev =
             var pair = A2(update, msg, model);
           |]
         else
-          [text|
-            try {
-              var pair = A2(update, msg, model);
-            } catch(err) {
-              if (isBackend) { bugsnag.notify(err); }
-              return;
-            }
-          |]
-
-    exportFns =
-      if isBackend
-        then
-          [text|
-            var fns =
-              { decodeWirePayloadHeader: $$author$$project$$LamderaHelpers$$decodeWirePayloadHeader
-              , decodeWireAnalytics: $$author$$project$$LamderaHelpers$$decodeWireAnalytics
-              , getUserModel : function() { return model.userModel }
-              }
-          |]
-        else
-          [text|
-            var fns = {}
-          |]
+          if isBackend
+            then
+              [text|
+                try {
+                  var pair = A2(update, msg, model);
+                } catch(err) {
+                  bugsnag.notify(err);
+                  return;
+                }
+              |]
+            else
+              [text|
+                try {
+                  var pair = A2(update, msg, model);
+                } catch(err) {
+                  return;
+                }
+              |]
 
     shouldProxy =
       onlyIf isLocalDev
@@ -208,10 +198,9 @@ injections isBackend isLocalDev =
           shouldProxy = $$author$$project$$LocalDev$$shouldProxy(msg)
         |]
   in
-  [text|
-
-    var isBackend = $isBackend_ && typeof isLamdera !== 'undefined';
-
+  if isBackend
+    then
+      [text|
     function _Platform_initialize(flagDecoder, args, init, update, subscriptions, stepperBuilder)
       {
         var result = A2(_Json_run, flagDecoder, _Json_wrap(args ? args['flags'] : undefined));
@@ -252,7 +241,7 @@ injections isBackend isLocalDev =
           const updateDuration = mtime() - start;
           start = mtime();
 
-          if (isBackend && loggingEnabled) {
+          if (loggingEnabled) {
             pos = pos + 1;
             const s = $$author$$project$$LBR$$serialize(msg);
             serializeDuration = mtime() - start;
@@ -268,26 +257,24 @@ injections isBackend isLocalDev =
           //console.log('cmds', pair.b);
           _Platform_enqueueEffects(managers, pair.b, subscriptions(model));
 
-          const stepEnqueueDuration = mtime() - start;
+          //const stepEnqueueDuration = mtime() - start;
 
-          if (isBackend) {
-            //console.log({serialize: serializeDuration, log: logDuration, update: updateDuration, stepEnqueue: stepEnqueueDuration})
-          }
+          //console.log({serialize: serializeDuration, log: logDuration, update: updateDuration, stepEnqueue: stepEnqueueDuration})
         }
 
         if ((args && args['model']) === undefined) {
           _Platform_enqueueEffects(managers, initPair.b, subscriptions(model));
         }
 
-        $exportFns
+        var fns =
+          { decodeWirePayloadHeader: $$author$$project$$LamderaHelpers$$decodeWirePayloadHeader
+          , decodeWireAnalytics: $$author$$project$$LamderaHelpers$$decodeWireAnalytics
+          , getUserModel : function() { return model.userModel }
+          }
 
         const die = function() {
           //console.log('App dying');
-
-          // Needed to stop Time.every subscriptions.
-          // This must be done before clearing the stuff below.
-          _Platform_enqueueEffects(managers, _Platform_batch(_List_Nil), _Platform_batch(_List_Nil));
-
+          // @TODO: It's unclear if nulling these does anything useful. What are we trying to achieve when dying on the backend?
           managers = null;
           model = null;
           stepper = null;
@@ -301,7 +288,208 @@ injections isBackend isLocalDev =
           fns: fns
         } : {};
       }
-  |]
+      |]
+    else
+      [text|
+    function _Platform_initialize(flagDecoder, args, init, update, subscriptions, stepperBuilder)
+      {
+        var result = A2(_Json_run, flagDecoder, _Json_wrap(args ? args['flags'] : undefined));
+
+        // @TODO need to figure out how to get this to automatically escape by mode?
+        //$$elm$$core$$Result$$isOk(result) || _Debug_crash(2 /**/, _Json_errorToString(result.a) /**/);
+        $$elm$$core$$Result$$isOk(result) || _Debug_crash(2 /**_UNUSED/, _Json_errorToString(result.a) /**/);
+
+        var managers = {};
+        var initPair = init(result.a);
+        var model = initPair.a;
+
+        // We'll temporarily overwrite these variables or functions.
+        var F2_backup = F2;
+        var _Browser_window_backup = _Browser_window;
+        var _VirtualDom_virtualize_backup = _VirtualDom_virtualize;
+        var _VirtualDom_applyPatches_backup = _VirtualDom_applyPatches;
+        var _VirtualDom_equalEvents_backup = _VirtualDom_equalEvents;
+
+        // stepperBuilder calls impl.setup() (if it exists, which it does only for
+        // Browser.application) as the first thing it does. impl.setup() returns the
+        // divertHrefToApp function, which is used to create the event listener for
+        // all <a> elements. That divertHrefToApp function is constructed using F2.
+        // Here we override F2 to store the listener on the DOM node so we can
+        // remove it later. _VirtualDom_virtualize is called a couple of lines
+        // later, so we use that to restore the original F2 function, so it can do
+        // the right thing when the view function is called.
+        F2 = function(f) {
+          return function(domNode) {
+            var listener = function(event) {
+              return f(domNode, event);
+            };
+            domNode.elmAf = listener;
+            return listener;
+          };
+        };
+
+        // To be able to remove the popstate and hashchange listeners.
+        var historyListenerCleanups = [];
+        _Browser_window = {
+          navigator: _Browser_window_backup.navigator,
+          addEventListener: function(eventName, listener) {
+            _Browser_window_backup.addEventListener(eventName, listener);
+            historyListenerCleanups.push(function() {
+              _Browser_window_backup.removeEventListener(eventName, listener);
+            });
+          },
+        };
+
+        // When passing in the last rendered VNode from a previous app:
+        if (args && args.vn) {
+          // Instead of virtualizing the existing DOM into a VNode, just use the
+          // one from the previous app. Html.map messes up Elm's
+          // _VirtualDom_virtualize, causing the entire thing inside the Html.map
+          // to be re-created even though it is already the correct DOM.
+          _VirtualDom_virtualize = function() {
+            F2 = F2_backup; // Restore F2 as mentioned above.
+
+            // It's a bit weird to mutate the args object that has been passed in,
+            // but the VNode from the previous app can have references to the old
+            // app functions through Html.lazy, preventing the old app from being
+            // garbage collected, so we don't want to keep a reference to it.
+            var lastVNode = args.vn;
+            delete args.vn;
+
+            return lastVNode;
+          };
+
+          _VirtualDom_applyPatches = function(rootDomNode, oldVirtualNode, patches, eventNode) {
+            if (patches.length !== 0) {
+              _VirtualDom_addDomNodes(rootDomNode, oldVirtualNode, patches, eventNode);
+            }
+            _VirtualDom_lastDomNode = _VirtualDom_applyPatchesHelp(rootDomNode, patches);
+            // Restore the event listeners on the <a> elements:
+            var aElements = _VirtualDom_lastDomNode.getElementsByTagName('a');
+            for (var i = 0; i < aElements.length; i++) {
+              var domNode = aElements[i];
+              domNode.addEventListener('click', _VirtualDom_divertHrefToApp(domNode));
+            }
+            return _VirtualDom_lastDomNode;
+          }
+
+          // Force all event listeners to be re-applied:
+          _VirtualDom_equalEvents = function() {
+            return false;
+          }
+        } else {
+          _VirtualDom_virtualize = function(node) {
+            F2 = F2_backup; // Restore F2 as mentioned above.
+            return _VirtualDom_virtualize_backup(node);
+          };
+        }
+
+        var stepper = stepperBuilder(sendToApp, model);
+
+        // Restore the original functions and variables.
+        F2 = F2_backup; // Should already be restored by now, but just in case.
+        _Browser_window = _Browser_window_backup;
+        _VirtualDom_virtualize = _VirtualDom_virtualize_backup;
+        _VirtualDom_applyPatches = _VirtualDom_applyPatches_backup;
+        _VirtualDom_equalEvents = _VirtualDom_equalEvents_backup;
+
+        var ports = _Platform_setupEffects(managers, sendToApp);
+
+        //console.log('managers', managers)
+        //console.log('ports', ports)
+
+        function sendToApp(msg, viewMetadata)
+        {
+          //console.log('sendToApp.active',msg);
+
+          $shouldProxy
+
+          $runUpdate
+
+          stepper(model = pair.a, viewMetadata);
+          //console.log('cmds', pair.b);
+          _Platform_enqueueEffects(managers, pair.b, subscriptions(model));
+        }
+
+        _Platform_enqueueEffects(managers, initPair.b, subscriptions(model));
+
+        const die = function() {
+          //console.log('App dying');
+
+          // Render one last time, synchronously, in case their is a scheduled
+          // render with requestAnimationFrame (which then become no-ops).
+          // Rendering mutates the vdom, and we want those mutations.
+          stepper(model, true /* isSync */);
+          var toReturn = _VirtualDom_lastVNode;
+
+          // Remove Elm's event listeners. Both the ones added
+          // automatically on every <a> element, as well as the ones
+          // added by using Html.Events.
+          var elements = _VirtualDom_lastDomNode.getElementsByTagName('*');
+          for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            if (element.elmAf) {
+              element.removeEventListener('click', element.elmAf);
+              delete element.elmAf;
+            }
+            if (element.elmFs) {
+              for (var key in element.elmFs) {
+                element.removeEventListener(key, element.elmFs[key]);
+              }
+              delete element.elmFs;
+            }
+          }
+
+          // Remove the popstate and hashchange listeners.
+          for (var i = 0; i < historyListenerCleanups.length; i++) {
+            historyListenerCleanups[i]();
+          }
+
+          // Clear these new things we've added.
+          _VirtualDom_lastVNode = null;
+          _VirtualDom_lastDomNode = null;
+          historyListenerCleanups = [];
+
+          // Stop rendering:
+          stepper = function() {};
+
+          // Note that subscriptions are turned off in Elm (by returning Sub.none)
+          // in upgrade mode, and the update function stops doing its regular business
+          // and just forwards messages instead.
+
+          return toReturn;
+        }
+
+        return ports ? {
+          ports: ports,
+          die: die,
+        } : {};
+      }
+
+    // Keep track of the last VNode rendered so we can pass it to the next app later.
+    var _VirtualDom_lastVNode = null;
+    function _VirtualDom_diff(x, y)
+    {
+      _VirtualDom_lastVNode = y;
+      var patches = [];
+      _VirtualDom_diffHelp(x, y, patches, 0);
+      return patches;
+    }
+
+    // Keep track of the reference to the latest root DOM node so we can perform cleanups in it later.
+    var _VirtualDom_lastDomNode = null;
+    function _VirtualDom_applyPatches(rootDomNode, oldVirtualNode, patches, eventNode)
+    {
+      if (patches.length === 0)
+      {
+        return (_VirtualDom_lastDomNode = rootDomNode);
+      }
+
+      _VirtualDom_addDomNodes(rootDomNode, oldVirtualNode, patches, eventNode);
+      return (_VirtualDom_lastDomNode = _VirtualDom_applyPatchesHelp(rootDomNode, patches));
+    }
+    }(this));
+      |]
 
   --   // https://github.com/elm/bytes/issues/20
   --   // but the fix below as suggested causes this problem:
