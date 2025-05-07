e97bc9d92a372a7a2512eeccc4abe882d8fec0ce
MartinSStewart <MartinSStewart@gmail.com>
2025-05-07 16:18:56 +0200

    Override LocalDev

--- terminal/src/Develop.hs
+++ terminal/src/Develop.hs
@@ -93,27 +93,13 @@ runWithRoot root (Flags maybePort) =
 
       elmJson <- Elm.Outline.read root True
 
-      atomicPutStrLn $
-        case elmJson of
-            Right (Elm.Outline.App (Elm.Outline.AppOutline _ _ direct _ _ _)) ->
-                case Map.lookup Lamdera.Project.lamderaProgramTest direct of
-                    Just (Elm.Version.Version major _ _) ->
-                        if major >= 4 then
-                            "Has program test"
-                        else
-                            "Version is too low"
-                    Nothing ->
-                        "Program test not installed"
-            Right (Elm.Outline.Pkg a) -> "elm.json is a package"
-            Left _ -> "elm.json not found"
-
       let
         recompile :: [String] -> IO ()
         recompile events = do
           -- Fork a recompile+cache update
           Sentry.asyncUpdateJsOutput sentryCache $ do
             debug_ $ "🛫  recompile triggered by: " ++ show events
-            harness <- Live.prepareLocalDev root
+            harness <- Live.prepareLocalDev (useLocalDevOverrides elmJson) root
             let
               typesRootChanged = events & filter (\event ->
                      stringContains "src/Types.elm" event
@@ -159,6 +145,21 @@ runWithRoot root (Flags maybePort) =
         <|> Live.serveUnmatchedUrlsToIndex root (serveElm sentryCache) -- Everything else without extensions goes to Lamdera LocalDev harness
         <|> error404 -- Will get hit for any non-matching extensioned paths i.e. /hello.blah
 
+useLocalDevOverrides :: Either a Elm.Outline.Outline -> Bool
+useLocalDevOverrides elmJson =
+    case elmJson of
+        Right (Elm.Outline.App (Elm.Outline.AppOutline _ _ direct _ _ _)) ->
+            case Map.lookup Lamdera.Project.lamderaProgramTest direct of
+                Just (Elm.Version.Version major _ _) ->
+                    if major >= 3 then
+                        True
+                    else
+                        False
+
+                Nothing ->
+                    False
+        _ ->
+            False
 
 -- Try narrow down source of exceptions with generalised error catch
 gcatchlog :: String -> Snap () -> Snap ()
