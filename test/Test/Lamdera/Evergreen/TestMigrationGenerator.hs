70c2a08a18b8148eccd1e8b21386a44f6268ab07
Hans Allis <hans@llis.nl>
2025-05-29 19:25:41 +0200

    Corrected argument order in TestMigrationGenerrator: actual and expected were reversed

--- test/Test/Lamdera/Evergreen/TestMigrationGenerator.hs
+++ test/Test/Lamdera/Evergreen/TestMigrationGenerator.hs
@@ -73,7 +73,7 @@ testMigrationGeneration scenario oldVersion newVersion = do
 
   _ <- io $ Lamdera.Relative.writeFile ("test/scenario-migration-generate/src/Evergreen/Migrate/VX" <> show newVersion <> ".elm") result
 
-  expectEqualTextTrimmed result (mock & withDefault "failed to load file")
+  expectEqualTextTrimmed (mock & withDefault "failed to load file") result
 
   let filenames =
         [ "src/Evergreen/V" <> show oldVersion <> "/Types.elm"
