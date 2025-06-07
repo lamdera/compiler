b6743992cd276fbb672d155c6ae599e31b4bdf5f
Rupert Smith <rupert@thesett.com>
2025-06-07 13:31:12 +0100

    Added Lamdera alternative implementation fo HTML generation only when building Lamdera project, not regular Elm.

--- compiler/src/Generate/Html.hs
+++ compiler/src/Generate/Html.hs
@@ -17,9 +17,50 @@ import qualified Lamdera.UiSourceMap
 
 -- SANDWICH
 
-
+-- @LAMDERA root :: FilePath parameter added.
 sandwich :: FilePath -> Name.Name -> B.Builder -> B.Builder
 sandwich root moduleName javascript =
+  Lamdera.alternativeImplementationWhen Lamdera.isLamdera_ (sandwich_ root moduleName javascript) $
+  let name = Name.toBuilder moduleName in
+  [r|<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>|] <> name <> [r|</title>
+  <style>body { padding: 0; margin: 0; }</style>
+</head>
+
+<body>
+
+<pre id="elm"></pre>
+
+<script>
+try {
+|] <> javascript <> [r|
+
+  var app = Elm.|] <> name <> [r|.init({ node: document.getElementById("elm") });
+}
+catch (e)
+{
+  // display initialization errors (e.g. bad flags, infinite recursion)
+  var header = document.createElement("h1");
+  header.style.fontFamily = "monospace";
+  header.innerText = "Initialization Error";
+  var pre = document.getElementById("elm");
+  document.body.insertBefore(header, pre);
+  pre.innerText = e;
+  throw e;
+}
+</script>
+
+</body>
+</html>|]
+
+
+-- @LAMDERA
+
+sandwich_ :: FilePath -> Name.Name -> B.Builder -> B.Builder
+sandwich_ root moduleName javascript =
   let
     name = Name.toBuilder moduleName
 
