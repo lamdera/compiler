490dd706400746c17a97149e3a4ca393d2831d4c
Mario Rogic <hello@mario.net.au>
2025-08-09 15:19:55 +1000

    Test Lamdera HTML injections don't happen on vanilla Elm projects

---
+++ test/Test/BackwardsCompat.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.BackwardsCompat where
+
+import EasyTest
+import Ext.Common
+import Lamdera hiding ((&))
+import Test.Helpers
+import qualified Lamdera.Compile
+
+suite :: Test ()
+suite = tests $
+  [ scope "an vanilla elm project compiled with lamdera should not inject lamdera html modifications" $
+    let
+      project = "./test/scenario-empty-elm-init"
+      bashInScenario c = bash $ "cd " ++ project ++ " && " ++ c
+
+      setup = do
+        rmdir $ project ++ "/elm-home"
+        rmdir $ project ++ "/elm-stuff"
+
+      cleanup _ = do
+        pure ()
+
+      test _ = do
+        io $ do
+          withEnvVars [("ELM_HOME", project ++ "/elm-home")] $
+            Lamdera.Compile.makeDevHtml project ["src/Main.elm"]
+
+        htmlM <- io $ readUtf8Text $ project ++ "/index.html"
+
+        case htmlM of
+          Nothing -> crash "index.html not found"
+          Just html -> do
+            expectTextDoesNotContain html "apple-mobile-web-app-capable"
+
+    in
+    using setup cleanup test
+  ]
