2434d1c9c56ccdec58381750b1e6a98b43b7b88a
Simon Lydell <simon.lydell@gmail.com>
2025-08-10 13:11:08 +0200

    Make Browser.Navigation.Key work

--- extra/Lamdera/Wire3/Decoder.hs
+++ extra/Lamdera/Wire3/Decoder.hs
@@ -349,7 +349,8 @@ decoderForType ifaces cname tipe =
 
 
     -- Frontend only JS reference types
-    TType (Module.Canonical (Name "elm" "browser") "Browser.Navigation") "Key" params -> callDecoder "decodeRef" tipe
+    -- See Injection.hs for why we don’t decode `Browser.Navigation.Key` as a reference.
+    -- TType (Module.Canonical (Name "elm" "browser") "Browser.Navigation") "Key" params -> callDecoder "decodeRef" tipe
     TType (Module.Canonical (Name "elm" "file") "File") "File" params -> callDecoder "decodeRef" tipe
     TType (Module.Canonical (Name "elm-explorations" "webgl") "WebGL.Texture") "Texture" params -> callDecoder "decodeRef" tipe
 
