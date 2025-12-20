6b1b76a405d3b51dbcb9733491c6cd080c5a717c
Simon Lydell <simon.lydell@gmail.com>
2025-12-20 14:21:50 +0100

    Add TODO comments

--- extra/Lamdera/PackageReplacements.hs
+++ extra/Lamdera/PackageReplacements.hs
@@ -12,13 +12,22 @@ type ReplacementMap = Map.Map ( Pkg.Name, ModuleName.Raw ) ByteString
 
 
 elm :: ReplacementMap
-elm = Map.empty
+elm =
+  -- TODO: Use TemplateHaskell to populate this map from extra/package-replacements/*
+  -- Use `git diff master... --name-only` to know which files need to be included
+  Map.empty
 
 
 kernel :: ReplacementMap
-kernel = Map.empty
+kernel =
+  -- TODO: Same here.
+  Map.empty
 
 
 get :: Pkg.Name -> ModuleName.Raw -> ReplacementMap -> IO ByteString -> IO ByteString
 get pkg name replacementMap original =
   maybe original return (Map.lookup (pkg, name) replacementMap)
+
+
+-- TODO: Also generate a map of package names and package versions used to validate that the
+-- user elm.json is compatible with our package replacements.
