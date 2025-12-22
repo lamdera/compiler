827678f27dba81ad6ff5abeb4b75fbb1d626c63a
Simon Lydell <simon.lydell@gmail.com>
2025-12-22 22:40:42 +0100

    Parse module names

--- extra/Lamdera/PackageReplacementsTH.hs
+++ extra/Lamdera/PackageReplacementsTH.hs
@@ -18,13 +18,15 @@ import Control.Monad
 import Data.List
 import qualified Data.Utf8 as Utf8
 import qualified Data.Name as Name
+import qualified Data.Maybe
 
 
 loadReplacements :: Q Exp
 loadReplacements = do
   submodules <- runIO findSubmodules
   entries    <- runIO (collectEntriesPerSubmodule submodules)
-  listE      <- mapM entryToExp entries
+  let entriesWithModuleNames = Data.Maybe.mapMaybe parseModuleName entries
+  listE      <- mapM entryToExp entriesWithModuleNames
   [| Map.fromList $(pure (ListE listE)) |]
 
 
@@ -62,17 +64,30 @@ collectEntriesPerSubmodule subs =
         | fp <- changed
         ]
 
+parseModuleName :: ((String, String), FilePath) -> Maybe ((String, String), FilePath, String)
+parseModuleName (authorProject, filePath) =
+  let
+    ext = takeExtension filePath
+  in
+  if ext == ".js" || ext == ".elm" then
+    case splitDirectories (dropExtension filePath) of
+        "extra" : "package-replacements" : _ : _ : "src" : rest ->
+            Just (authorProject, filePath, Data.List.intercalate "." rest)
+
+        _ ->
+            Nothing
+
+  else
+    Nothing
+
 bs :: ByteString
 bs = "test"
 
 entryToExp
-  :: ((String, String), FilePath)
+  :: ((String, String), FilePath, String)
   -> Q Exp
-entryToExp ((author, project), path) = do
+entryToExp ((author, project), path, moduleName) = do
   -- bs <- runIO (Data.ByteString.readFile path)
-
-  -- let moduleName = dropExtension (takeFileName path)
-  let moduleName = path
 
   [|
     ( ( Pkg.toName (Utf8.fromChars author) project
