f19f5ac3c7caa2683b72b87c905a64dded03dede
Evan Czaplicki <evancz@users.noreply.github.com>
2026-04-27 12:12:09 +0200

    remove unused imports

--- terminal/src/Develop/StaticFiles.hs
+++ terminal/src/Develop/StaticFiles.hs
@@ -11,9 +11,7 @@ module Develop.StaticFiles
 
 import Prelude hiding (lookup)
 import qualified Data.ByteString as BS
-import Data.FileEmbed (bsToExp)
 import qualified Data.HashMap.Strict as HM
-import Language.Haskell.TH (runIO)
 import System.FilePath ((</>))
 
 import qualified Literals
